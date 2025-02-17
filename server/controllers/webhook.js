bcc68b1d0b1bec3a77f69aa2253351be2b04f6f5
Pritiranjan <patrapritiranjan11@gmail.com>
2025-02-17 12:00:51 +0530

    Email Address

--- server/controllers/webhook.js
+++ server/controllers/webhook.js
@@ -14,7 +14,7 @@ import User from "../models/User.js";
         case 'user.created':{
             const userData={
                 _id:data.id,
-                email:data.email_addresses[0].email_addresses,
+                email:data.email_addresses[0].email_address,
                 name:data.first_name +" "+data.last_name,
                 image:data.image_url,
                 resume:''
