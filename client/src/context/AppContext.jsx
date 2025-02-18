d81358058565b5c88698a10c0d624f1ffc92718c
Pritiranjan <patrapritiranjan11@gmail.com>
2025-02-19 00:16:41 +0530

    Project Completed

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,8 @@
 import { createContext, useEffect, useState } from "react";
-import { jobsData } from "../assets/assets";
+
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext();
 export const AppContextProvider = (props) => {
@@ -7,17 +10,101 @@ export const AppContextProvider = (props) => {
     title: "",
     location: "",
   });
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const { user } = useUser();
+  const { getToken } = useAuth();
   const [isSearched, setIsSearched] = useState(false);
   const [jobs, setJobs] = useState([]);
-  const [showRecruiterLogin,setShowRecruiterLogin]=useState(false);
+  const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
+  const [companyToken, setCompanyToken] = useState(null);
+  const [companyData, setCompanyData] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [userApplications, setUserApplications] = useState([]);
 
   //Function to fetch jobs
   const fetchJobs = async () => {
-    setJobs(jobsData);
+    try {
+      const { data } = await axios.get(backendUrl + "/api/jobs");
+      if (data.success) {
+        setJobs(data.jobs);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
+  const fetchCompanyData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/company/company", {
+        headers: { token: companyToken },
+      });
+      if (data.success) {
+        setCompanyData(data.company);
+        console.log(data);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(data.message);
+    }
+  };
+
+  const fetchUserData = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.get(backendUrl + "/api/users/user", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (data.success) {
+        setUserData(data.user);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const fetchUserApplications =async ()=>{
+    try {
+      const token =await getToken();
+      const {data}=await axios.get(backendUrl+'/api/users/applications',
+        {
+          headers:{Authorization:`Bearer ${token}`}
+        }
+      )
+      if(data.success){
+        setUserApplications(data.applications)
+      }else{
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message);
+      
+    }
+  }
+
   useEffect(() => {
     fetchJobs();
+    const storedCompanyToken = localStorage.getItem("companyToken");
+    if (storedCompanyToken) {
+      setCompanyToken(storedCompanyToken);
+    }
   }, []);
+  useEffect(() => {
+    if (companyToken) {
+      fetchCompanyData();
+    }
+  }, [companyToken]);
+
+  useEffect(() => {
+    if (user) {
+      fetchUserData();
+      fetchUserApplications();
+    }
+  }, [user]);
 
   const value = {
     searchFilter,
@@ -27,7 +114,19 @@ export const AppContextProvider = (props) => {
     jobs,
     setJobs,
     showRecruiterLogin,
-    setShowRecruiterLogin
+    setShowRecruiterLogin,
+    companyToken,
+    setCompanyToken,
+    companyData,
+    setCompanyData,
+    backendUrl,
+    setUserData,
+    userData,
+    userApplications,
+    setUserApplications,
+    fetchUserData,
+    fetchUserApplications,
+
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import Quill from 'quill' 
 import { JobCategories, JobLocations } from '../assets/assets'
+import axios from 'axios'
+import { AppContext } from '../context/AppContext'
+import { toast } from 'react-toastify'
 function AddJob() {
 
     const [title,setTitle] = useState('')
@@ -10,6 +13,39 @@ function AddJob() {
     const [salary,setSalary]=useState(0)
     const editorRef=useRef(null);
     const quillRef=useRef(null);
+    const {backendUrl,companyToken}=useContext(AppContext)
+
+    const onSubmitHandler=async (e)=>{
+      e.preventDefault();
+      try {
+        const description=quillRef.current.root.innerHTML;
+        const {data}=await axios.post(backendUrl+'/api/company/post-job',{
+          title,
+          description,
+          location,
+          salary,
+          category,
+          level
+
+        },{
+          headers:{token:companyToken}
+        }
+      )
+      if(data.success){
+        toast.success(data.message);
+        setTitle("")
+        setSalary(0)
+        quillRef.current.root.innerHTML=""
+
+      }else{
+        toast.error(data.message)
+      }
+      } catch (error) {
+        toast.error(error.message)
+        
+      }
+
+    }
     useEffect(()=>{
       if(!quillRef.current && editorRef.current){
         quillRef.current=new Quill(editorRef.current,{
@@ -18,7 +54,7 @@ function AddJob() {
       }
     },[])
   return (
-    <form className='container p-4 flex flex-col w-full items-start gap-3'>
+    <form onSubmit={onSubmitHandler} className='container p-4 flex flex-col w-full items-start gap-3'>
       <div className='w-full '>
         <p className='mb-2'>Job Title</p>
         <input type="text"placeholder='Type here' onChange={e=>setTitle(e.target.value)} value={title} required  className='w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded'/>
@@ -67,9 +103,9 @@ function AddJob() {
       </div>
       <div>
           <p className='mb-2'>Job Salary</p>
-          <input min={0} className='w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]' onChange={e=>setSalary(e.target.value)} type="number" placeholder='25000' />
+          <input min={0} className='w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]' value={salary} onChange={e=>setSalary(e.target.value)} type="number" placeholder='25000' />
         </div>
-        <button className='w-28 py-3 mt-4 bg-black text-white rounded'>Add</button>
+        <button type='submit' className='w-28 py-3 mt-4 bg-black text-white rounded cursor-pointer'>Add</button>
     </form>
   )
 }
